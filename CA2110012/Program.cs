8ad764b37a900a964264a198d53d121c01dbc141
Samu Dániel Botond <samu.daniel@verebely.dc>
2021-10-12 08:35:16 +0200

    Add project files.

---
+++ CA2110012/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2110012
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
